e4c10a0b3778112fcb4b1cfc1f7d9074c2ed6983
venkatanaveenkumarnuthalapati <venkatanaveenkumarnuthalapati@gmail.com>
2023-08-15 10:25:57 +0530

    commit13

--- ibank.jsx
+++ ibank.jsx
@@ -11,18 +11,20 @@ app.use(bodyParser.json());
 //////////////////////// SqlServer DB connection /////////////////////////
 const mssql = require('mssql');
 
+let request;
 const getConnection = async () => {
     console.log(config.sql);
     try {
         // await mssql.connect(config.sql);
         await mssql.connect(config.sql);
         console.log('Connected to SQL Server successfully!');
-        return new mssql.Request();
+        request = new mssql.Request();
     } catch (error) {
         console.error('Error connecting to SQL Server:', error);
         throw new Error('Error connecting to the database');
     }
 };
+getConnection();
 
 const closeConnection = () => {
     mssql.close();
@@ -32,46 +34,49 @@ const closeConnection = () => {
 ////////////////// API 1 /////////////////////////
 app.get('/', async (req, res) => {
     try {
-        const request = await getConnection();
+        // const request = await getConnection();
         const records = await request.query(`SELECT * FROM AccountMaster;`);
         res.status(200).send(records.recordset);
     } catch (queryError) {
         console.error('Error executing SQL query:', queryError);
         res.status(500).send(`Error executing SQL query ${queryError}`);
-    } finally {
-        closeConnection();
     }
+    // finally {
+    //     closeConnection();
+    // }
 });
 
-////////////////// API 1 /////////////////////////
+////////////////// API 2 /////////////////////////
 app.get('/login/:accNum', async (req, res) => {
     const accNum = req.params.accNum; // Access the parameter using req.params
 
     try {
-        const request = await getConnection();
+        // const request = await getConnection();
         const records = await request.query(`SELECT * FROM AccountMaster WHERE ACID = ${accNum};`);
         res.status(200).send(records.recordset);
     } catch (queryError) {
         console.error('Error executing SQL query:', queryError);
         res.status(500).send('Error executing SQL query');
-    } finally {
-        closeConnection();
     }
+    // finally {
+    //     closeConnection();
+    // }
 });
 
 ////////////////// API 2 /////////////////////////
 app.get('/statement/:accNum', async (req, res) => {
     const accNum = req.params.accNum; // Access the parameter using req.params
     try {
-        const request = await getConnection();
+        // const request = await getConnection();
         const records = await request.query(`select * , 0 as TotalBal from TransactionMaster WHERE ACID = ${accNum};`);
         res.status(200).send(records.recordset);
     } catch (queryError) {
         console.error('Error executing SQL query:', queryError);
         res.status(500).send('Error executing SQL query');
-    } finally {
-        closeConnection();
     }
+    // finally {
+    //     closeConnection();
+    // }
 });
 
 const server = app.listen(config.port, () => {
